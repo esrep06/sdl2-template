659b7fad39fa0a595ed75e68a8d7a0dfdd017b4d
esrep06 <esrep06@gmail.com>
2024-05-15 15:38:35 -0400

    Added cpp-utilz as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() 
 {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         std::cout << "Failed to initialize the SDL2 library\n";
         return -1;
@@ -15,17 +15,11 @@ int main()
             680, 480,
             0);
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
     if(!window)
     {
         std::cout << "Failed to create window\n";
-        return -1;
-    }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if(!window_surface)
-    {
-        std::cout << "Failed to get the surface from the window\n";
         return -1;
     }
 
@@ -45,8 +39,15 @@ int main()
             }
         }
 
-        SDL_UpdateWindowSurface(window);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+
+        SDL_RenderPresent(renderer);
     }
+
+    SDL_Quit();
 
     return 0;
 }
